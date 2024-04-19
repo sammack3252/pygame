99b43aa1ac66394205cafdc125d2fdb2c2ee4192
intensecheese <146556801+sammack3252@users.noreply.github.com>
2024-04-19 14:10:11 -0500

    Update main.py
    added other icons and fixed their position

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 
 scriptDir = os.path.dirname(os.path.abspath(__file__))
 
-class Item:
+class Button: #used for icons
     def __init__(self, x, y, image_name):
         self.x = x
         self.y = y
@@ -25,17 +25,31 @@ class Game:
         self.clock_tick = 60
         self.clock = pygame.time.Clock()
 
+        #icons
         foodIcon = pygame.image.load(os.path.join(scriptDir, "graphics", "Icon_Food.png"))
+        foodIcon = pygame.transform.scale(foodIcon, (75, 75))
+        self.food_button = Button(self.width/8, self.buttons_bar_height/2, foodIcon)
+
         cartIcon = pygame.image.load(os.path.join(scriptDir, "graphics", "Icon_Cart.png"))
+        cartIcon = pygame.transform.scale(cartIcon, (75, 75))
+        self.cart_button = Button((self.width/8 * 3), self.buttons_bar_height/2, cartIcon)
+
         hangerIcon = pygame.image.load(os.path.join(scriptDir, "graphics", "Icon_Hanger.png"))
- 
-        self.image_names = [foodIcon, cartIcon, hangerIcon]
-        self.food_button = Item(self.width/4, self.buttons_bar_height/2, foodIcon)
-    
+        hangerIcon = pygame.transform.scale(hangerIcon, (75, 75))
+        self.hanger_button = Button((self.width/8 * 5), self.buttons_bar_height/2, hangerIcon)
+
+        statsIcon = pygame.image.load(os.path.join(scriptDir, "graphics", "Icon_Stats.png"))
+        statsIcon = pygame.transform.scale(statsIcon, (75, 75))
+        self.stats_button = Button((self.width/8 * 7), self.buttons_bar_height/2, statsIcon)
+
     def draw_everything(self):
         self.screen.fill(self.background_color)
         pygame.draw.rect(self.screen, self.buttons_bar_color, pygame.Rect(0, 0, self.width, self.buttons_bar_height))
         self.screen.blit(self.food_button.image, self.food_button.image_rect)
+        self.screen.blit(self.cart_button.image, self.cart_button.image_rect)
+        self.screen.blit(self.hanger_button.image, self.hanger_button.image_rect)
+        self.screen.blit(self.stats_button.image, self.stats_button.image_rect)
+
         pygame.display.update()
     
     def run(self):
