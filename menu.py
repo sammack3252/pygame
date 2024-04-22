a1f650d2bc1762ef602db311beab1ac43d9d5069
intensecheese <146556801+sammack3252@users.noreply.github.com>
2024-04-22 14:30:53 -0500

    menu wip
    wip ok its kinda poopy rn

---
+++ menu.py
@@ -0,0 +1,92 @@
+import pygame
+import pygame.freetype
+from pygame.sprite import Sprite
+from pygame.rect import Rect
+
+BLUE = (106, 159, 181)
+WHITE = (255, 255, 255)
+
+def create_surface_with_text(text, font_size, text_rgb, bg_rgb):
+    #returns surface with text written on
+    font = pygame.freetype.SysFont("Courier", font_size, bold=True)
+    surface, _= font.render(text=text, fgcolor=text_rgb, bgcolor=bg_rgb)
+    return surface.convert_alpha()
+
+class UIElement(Sprite):
+    #a user interface element that can be added to a surface
+    def __init__(self, center_position, text, font_size, bg_rgb, text_rgb):
+        '''
+        args:
+            center_position - tuple (x, y)
+            text - string of text to write
+            font_size - int
+            bg_rgb (background color) - tuple (r, g, b)
+            text_rgb (text color) - tuple (r, g, b)
+        '''
+        self.mouse_over = False #indicates if the mouse is over the element
+
+        #create the default image
+        default_image = create_surface_with_text(
+            text=text, font_size=font_size * 1.2, text_rgb=text_rgb, bg_rgb=bg_rgb
+        )
+        
+        #create the image that shows when mouse is over the element
+        highlighted_image = create_surface_with_text(
+            text=text, font_size=font_size * 1.2, text_rgb=text_rgb, bg_rgb=bg_rgb
+        )
+
+        #add both images and their rects to lists
+        self.images = [default_image, highlighted_image]
+        self.rects = [
+            default_image.get_rect(center=center_position),
+            highlighted_image.get_rect(center=center_position),
+        ]
+
+        #calls the init method of the parent sprite class
+        super().__init__()
+
+        #properties that vary the image and its rect when the mouse is over the element
+    @property
+    def image(self):
+        return self.images[1] if self.mouse_over else self.images[0]
+        
+    @property
+    def rect(self):
+        return self.rects[1] if self.mouse_over else self.rects[0]
+        
+    def update(self, mouse_pos):
+        if self.rect.collidepoint(mouse_pos):
+            self.mouse_over = True
+        else:
+            self.mouse_over = False
+        
+    def draw(self, surface):
+        #draws element onto a surface
+        surface.blit(self.image, self.rect)
+        
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((800, 600))
+
+    #create a ui element
+    uielement = UIElement(
+        center_position=(400, 400),
+        font_size=30,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Hello World",
+    )
+
+    #main loop
+    while True:
+        for event in pygame.event.get():
+            pass
+        screen.fill(BLUE)
+        uielement.update(pygame.mouse.get_pos())
+        uielement.draw(screen)
+        pygame.display.flip()
+        
+#call main when the script is run
+if __name__ == "__main__":
+    main()
