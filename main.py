36c3fd803128de622858b4e3d8b5f3e80d0bcacf
intensecheese <146556801+sammack3252@users.noreply.github.com>
2024-04-24 14:31:39 -0500

    merged menu and game files, but menu still wip poopy rn
    the menu is doodoo rn but its ok we're learning :relieved:

--- main.py
+++ main.py
@@ -1,9 +1,142 @@
 import os.path
 import pygame
+import pygame.freetype
+from pygame.sprite import Sprite
+from pygame.rect import Rect
+from enum import Enum
 
 scriptDir = os.path.dirname(os.path.abspath(__file__))
 
-class Button: #used for icons
+BLUE = (106, 159, 181)
+WHITE = (255, 255, 255)
+
+
+def create_surface_with_text(text, font_size, text_rgb, bg_rgb):
+    #returns surface with text written on
+    font = pygame.freetype.SysFont("Courier", font_size, bold=True)
+    surface, _ = font.render(text=text, fgcolor=text_rgb, bgcolor=bg_rgb)
+    return surface.convert_alpha()
+
+
+class UIElement(Sprite):
+    #a user interface element that can be added to a surface
+    def __init__(self, center_position, text, font_size, bg_rgb, text_rgb, action=None):
+        """
+        args:
+            center_position - tuple (x, y)
+            text - string of text to write
+            font_size - int
+            bg_rgb (background colour) - tuple (r, g, b)
+            text_rgb (text colour) - tuple (r, g, b)
+            action - the gamestate change associated with this button
+        """
+        self.mouse_over = False #indicates if the mouse is over the element
+
+        #create the default image
+        default_image = create_surface_with_text(
+            text=text, font_size=font_size, text_rgb=text_rgb, bg_rgb=bg_rgb
+        )
+
+        #create the image that shows when mouse is over the element
+        highlighted_image = create_surface_with_text(
+            text=text, font_size=font_size * 1.2, text_rgb=text_rgb, bg_rgb=bg_rgb
+        )
+
+        #add both images and their rects to lists
+        self.images = [default_image, highlighted_image]
+        self.rects = [
+            default_image.get_rect(center=center_position),
+            highlighted_image.get_rect(center=center_position),
+        ]
+
+        #assign button action
+        self.action = action
+
+        #calls the init method of the parent sprite class
+        super().__init__()
+
+#properties that vary the image and its rect when the mouse is over the element
+    @property
+    def image(self):
+        return self.images[1] if self.mouse_over else self.images[0]
+
+    @property
+    def rect(self):
+        return self.rects[1] if self.mouse_over else self.rects[0]
+
+    def update(self, mouse_pos, mouse_up):
+        #updates the elemtn's appearance depending on the mouse position and returns the button's action if clicked
+        if self.rect.collidepoint(mouse_pos):
+            self.mouse_over = True
+            if mouse_up:
+                return self.action
+        else:
+            self.mouse_over = False
+
+    def draw(self, surface):
+        #draws element onto a surface
+        surface.blit(self.image, self.rect)
+
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((500, 500))
+    pygame.display.set_caption("Python Pal")
+    game_state = GameState.TITLE
+
+    while True:
+        if game_state == GameState.TITLE:
+            game_state = title_screen(screen)
+
+        if game_state == GameState.NEWGAME:
+            game_state = play(screen)
+
+        if game_state == GameState.QUIT:
+            pygame.quit()
+            return
+
+
+def title_screen(screen):
+    start_btn = UIElement(
+        center_position=(250, 300),
+        font_size=30,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Start",
+        action=GameState.NEWGAME,
+    )
+    quit_btn = UIElement(
+        center_position=(250, 400),
+        font_size=30,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Quit",
+        action=GameState.QUIT,
+    )
+
+    buttons = [start_btn, quit_btn]
+
+    #main loop
+    while True:
+        mouse_up = False
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                mouse_up = True
+        screen.fill(BLUE)
+
+        for button in buttons:
+            ui_action = button.update(pygame.mouse.get_pos(), mouse_up)
+            if ui_action is not None:
+                return ui_action
+            button.draw(screen)
+
+        pygame.display.flip()
+
+
+#everything to do w the actual game vv
+
+class Button:
     def __init__(self, x, y, image_name):
         self.x = x
         self.y = y
@@ -62,6 +195,40 @@ class Game:
             self.draw_everything()
             self.clock.tick(self.clock_tick)
 
-pygame.init()
-game = Game()
-game.run()
+def play(screen):
+    return_btn = UIElement(
+        center_position=(140, 570),
+        font_size=20,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Return to main menu",
+        action=GameState.TITLE,
+    )
+    game = Game()
+    game.run()
+
+    while True:
+        mouse_up = False
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                mouse_up = True
+        screen.fill(BLUE)
+
+        ui_action = return_btn.update(pygame.mouse.get_pos(), mouse_up)
+        if ui_action is not None:
+            return ui_action
+        return_btn.draw(screen)
+
+        pygame.display.flip()
+
+#end massive game chunk ^^
+
+
+class GameState(Enum):
+    QUIT = -1
+    TITLE = 0
+    NEWGAME = 1
+
+#call main when the script is run
+if __name__ == "__main__":
+    main()
